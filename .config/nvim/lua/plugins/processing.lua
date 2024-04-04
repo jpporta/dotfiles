9baa95dca24cfa421740df8a7671ef785254faa5
Joao Pedro Pin Porta <jpedro.porta@gmail.com>
2024-04-04 07:07:05 -0300

    remove processing + remove rest

--- .config/nvim/lua/plugins/processing.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"sophacles/vim-processing",
-	dependencies = { "skywind3000/asyncrun.vim" },
-	lazy = true,
-	ft = "processing",
-	config = function()
-		vim.keymap.set("n", "<leader>oo", "<cmd>AsyncRun -program=make @<CR>", { desc = "Run Sketch" })
-	end,
-}
